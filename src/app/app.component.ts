0c34956d2f04b1c906c72b07392b671f40645026
Matheus Mendes <103116527+M3NT0Sz@users.noreply.github.com>
2026-01-07 11:08:07 -0300

    Finalizando o curso 'Angular: construa uma aplicação web com componentes, linguagem de template e CLI', usando FormsModule e NgModel e fazendo o desafio de filtrar por nome sem acentos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { ContainerComponent } from './componentes/container/container.component'
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
 import { ContatoComponent } from './componentes/contato/contato.component';
+import { FormsModule } from '@angular/forms';
 
 interface Contato {
   id: number;
@@ -22,6 +23,7 @@ import agenda from './agenda.json';
     CabecalhoComponent,
     SeparadorComponent,
     ContatoComponent,
+    FormsModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
@@ -30,9 +32,24 @@ export class AppComponent {
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
   contatos: Contato[] = agenda;
 
-  filtrarContatosPorLetraInicial(letra: string): Contato[] {
+  filtroPorTexto: string = '';
+
+  filtrarContatosPorTexto(): Contato[] {
+    if (!this.filtroPorTexto) {
+      return this.contatos;
+    }
     return this.contatos.filter(contato => {
+      const nomeSemAcento = contato.nome.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+      const filtroSemAcento = this.filtroPorTexto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+      return nomeSemAcento
+        .toLowerCase()
+        .includes(filtroSemAcento.toLowerCase());
+    });
+  }
+
+  filtrarContatosPorLetraInicial(letra: string): Contato[] {
+    return this.filtrarContatosPorTexto().filter((contato) => {
       return contato.nome.toLowerCase().startsWith(letra);
-    })
+    });
   }
 }
